e83a13f2e3dea5c1f1766519755e2ab685c22b91
Lukas Kreibig <78150333+lukaskreibig@users.noreply.github.com>
2022-03-26 19:38:21 +0100

    feat: select generation filter buttons added

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { gql, useQuery } from '@apollo/client';
 import Search from './components/Search';
 import heart from './assets/heart.png'
 import noheart from './assets/noheart.png'
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 
 
@@ -15,6 +16,43 @@ const [storage, setStorage] = useState<any>([])
 const [search, setSearch] = useState<boolean>(false)
 const [searchResults, setSearchResults] = useState<any>([])
 const [loadingSearch, setLoadingSearch] = useState<boolean>(false)
+const [onMouseOver, setOnMouseOver] = useState<boolean>(false);
+const [generation] = useState<any>([
+    {
+      id: 1,
+      range: {from: 0, to: 151}
+    },
+    {
+      id: 2,
+      range: {from: 152, to: 251}
+    },
+    {
+      id: 3,
+      range: {from: 252, to: 386}
+    },
+    {
+      id: 4,
+      range: {from: 387, to: 493}
+    },
+    {
+      id: 5,
+      range: {from: 494, to: 649}
+    },
+    {
+      id: 6,
+      range: {from: 650, to: 721}
+    },
+    {
+      id: 7,
+      range: {from: 722, to: 809}
+    },
+    {
+      id: 8,
+      range: {from: 810, to: 905}
+    },
+  ]);
+
+  const [selected, setSelected] = useState<any>([])
 
   const GET_POKEMON = gql`
   query pokemons($limit: Int, $offset: Int) {
@@ -49,23 +87,22 @@ const [loadingSearch, setLoadingSearch] = useState<boolean>(false)
   }, [storage]);
 
 
-  const triggerLike = (x:any, heart?:any):void => {
+  const triggerLike = (y:any, heart?:any):void => {
     if (!heart)  {
-    setStorage([...storage, x])
+    setStorage([...storage, y])
     } else {
-    setStorage(storage.filter((obj: { id: any; }) => obj.id !== x.id))
+    setStorage(storage.filter((obj: { id: any; }) => obj.id !== y.id))
     }
   }
 
-  const { loading, error, data } = useQuery(GET_POKEMON, {variables: {limit: 20}} );
+  const { loading, error, data } = useQuery(GET_POKEMON, {variables: {limit: 300}} );
   // (error ? (console.log(`Error! ${error.message}`)) : null)
 
   let newPokemon:any = null
   if (data) {newPokemon = [...data.pokemons.results]}
 
-  let placeholder
+  let placeholder:any
   favourite ? placeholder = storage : search && searchResults ? placeholder = searchResults : placeholder = newPokemon
-
 
   let handleSearch = (results:any):void => {
     if(results[0].id !== null){
@@ -81,9 +118,44 @@ const [loadingSearch, setLoadingSearch] = useState<boolean>(false)
     }
     console.log("Passing up results", results)
   }
+console.log("Placeholder", placeholder)
+console.log("selected", selected)
+
+  const onChange = (id:any) => {
+    let find = selected.indexOf(id)
+
+    if(find > -1) {
+      selected.splice(find, 1)
+    } else {
+      selected.push(id)
+    }
+    setSelected(selected)
+  }
 
   
-  console.log("Placeholder", placeholder)
+  let finalarray:any = []
+
+
+  // const dataJuggle = () => {
+
+    // console.log("DataJugglestarts", placeholder)
+    // let yolo2:any = 0
+    // selected.map((number:any) => 
+    //     yolo2 = placeholder.filter((poke:any) => poke.id === number.id,
+    //     console.log("Yolo2", yolo2)) )}
+
+    //  placeholder = generation.map((gen:any) => {
+    //   let pancake = sorted.filter((item: { id:number; }) => {item.id  > gen.range.from && item.id < gen.range.to; console.log("Object names when sorting", item.id, gen.range.from, gen.range.to);});
+    //   finalarray.push(pancake); 
+    //   console.log("the new array the new array", finalarray)
+    //   console.log("Was passiert im pancake?", pancake)
+    //   })
+    //   console.log("DataJuggle has filtered", placeholder)
+
+      
+// useEffect(():void => {
+//   dataJuggle()
+// }, [loading]);
 
   return (
     <div className="app">
@@ -95,13 +167,36 @@ const [loadingSearch, setLoadingSearch] = useState<boolean>(false)
 
     <Search handleSearch={handleSearch} />
 
-    <li className={"searchbar"} id={null ?? "searchbar-clicked"}> Filter</li>
+    <li className={"searchbar"} id={(onMouseOver || selected.length ? "filter" : "")} onMouseEnter={() => setOnMouseOver(true)} onMouseLeave={() => setOnMouseOver(false)}> 
+          <div className={"filterheader"}>Generation</div>
+          {onMouseOver || selected.length ? 
+          <>
+          <div className='checkdiv'>
+
+          {generation.map((item:any) => {
+          return (
+          <label key={item.id}>{item.id}
+          <input type="checkbox" id="check"
+            onChange={() => onChange(item.id)}
+            selected={selected.includes(item.id)}
+          ></input>
+          
+          </label>
+          )})}
+          </div>
+          </>
+          
+          : <></> }
+          
+    </li>
+
+
     <li className={"searchbar"} onClick={() => setSort(!sort)}> {sort ? "Sort from Z-A" : "Sort from A-Z"} </li>
     <li className={"searchbar"} id={favourite ? "searchbar-clicked" : undefined} onClick={() => setFavourite(!favourite)}> Favourites</li>
+  
       {
-        placeholder
-        // .sort((a: { name: string; }, b: { name: string; }) => sort ? b.name.localeCompare(a.name) : a.name.localeCompare(b.name))
-
+        placeholder.sort((a: { name: string; }, b: { name: string; }) => sort ? b.name.localeCompare(a.name) : a.name.localeCompare(b.name))
+        // .filter(poke => poke.id === 63)
         .map((x:any, index:number) => { 
          return (
          <li className="gridchild" key={index}>
@@ -132,3 +227,4 @@ const [loadingSearch, setLoadingSearch] = useState<boolean>(false)
 }
 
 export default App;
+
